85e75a19b088511889cbd5459e65f094beeafaf0
Greg Hale <ImAlsoGreg@gmail.com>
2013-10-25 09:18:48 -0400

    working cluster parser, sans gain modification

--- lib/Data/Ephys.hs
+++ lib/Data/Ephys.hs
@@ -1,11 +1,13 @@
 module Data.Ephys (
-  module Data.Ephys.OldMWL.FileInfo,
-  module Data.Ephys.OldMWL.Parse,
   module Data.Ephys.Spike,
-  module Data.Ephys.EphysDefs
+  module Data.Ephys.EphysDefs,
+  module Data.Ephys.OldMWL.FileInfo,
+  module Data.Ephys.OldMWL.Parse,  
+  module Data.Ephys.OldMWL.ParseClusterFile
   ) where
 
 import Data.Ephys.EphysDefs
 import Data.Ephys.Spike
 import Data.Ephys.OldMWL.FileInfo
 import Data.Ephys.OldMWL.Parse
+import Data.Ephys.OldMWL.ParseClusterFile
--- lib/Data/Ephys/OldMWL/ParseClusterFile.hs
+++ lib/Data/Ephys/OldMWL/ParseClusterFile.hs
@@ -5,18 +5,11 @@ import Data.Ephys.Cluster
 import Data.Map
 import Text.Parsec
 import Control.Monad (liftM, replicateM)
-import Control.Applicative ((<$),(<*),(*>),(<*>), liftA)
+import Control.Applicative ((<$),(<*),(*>),(<*>),(<$>), liftA)
 import Data.Char           (chr)
 
 parseClusters :: String -> Either ParseError (Map Int ClusterMethod)
 parseClusters = parse pClusterFile "Cluster file"
-aoeu 
-pClusterFile :: Parsec String () (Map Int ClusterMethod)
-pClusterFile = do
-  many pHeader
-  assoc <- many pProjectionPoly
-  eof
-  return $ foldList  assoc
 
 foldList :: [(Int, CartBound)] -> Map Int ClusterMethod
 foldList bs = Prelude.foldl appendByKey empty bs
@@ -31,6 +24,13 @@ pHeader :: Parsec String () (Map Int ClusterMethod)
 pHeader = char '%' *> many (noneOf "\n") *> newline
 -}
 
+pClusterFile :: Parsec String () (Map Int ClusterMethod)
+pClusterFile = do
+  many pHeader
+  assoc <- many pProjectionPoly
+  eof
+  return $ foldList  assoc
+
 pHeader :: Parsec String st ()
 pHeader = do
   char '%'
@@ -40,21 +40,23 @@ pHeader = do
 
 pProjectionPoly :: Parsec String () (Int, CartBound)
 pProjectionPoly = do
-  clustId <- read `liftM` many digit
   newline
-  proj1 <- read `liftM` many digit
-  spaces
-  proj2 <- read `liftM` many digit
-  _ <- many (noneOf " \n\t")
+  clustId <- read `liftM` many1 digit
   newline
-  _ <- many (noneOf " \n\t")
+  proj1 <- read `liftM` many1 digit
+  char '\t'
+  proj2 <- read `liftM` many1 digit
   newline
-  nPoint <- read `liftM` many digit
-  many (noneOf "\n") >> newline
+  many (noneOf "\n\t")
+  newline
+  many (noneOf "\n\t")
+  newline
+  nPoint <- read `liftM` many1 digit
+  many (noneOf "\n")
+  newline
   pts <- replicateM nPoint $ do
---    x <- read `liftM` many digit <*> char '.' <*> many digit
     x <- pDouble
-    spaces
+    char '\t'
     y <- pDouble
     newline
     return (x,y)
