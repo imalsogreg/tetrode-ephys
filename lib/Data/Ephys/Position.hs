240e203087f713b3c9eeddc2d34c4434b01e821b
Greg Hale <imalsogreg@gmail.com>
2013-10-31 21:47:53 -0400

    Realized how tricky 1.5D track might be to represent.  Going for a graph.

--- lib/Data/Ephys/Position.hs
+++ lib/Data/Ephys/Position.hs
@@ -16,8 +16,22 @@ data Angle = Angle {_yaw :: Double, _pitch :: Double, _roll :: Double}
 
 $(makeLenses ''Angle)
 
-data Position = Position {_location :: Location, _angle :: Angle}
+data Heading = Heading {_dxdt :: Double, _dydt :: Double, _dzdt :: Double}
+               deriving (Eq, Show)
+$(makeLenses ''Heading)
+
+data PosConf = Unsure | Sure
+             deriving (Eq, Ord, Show)
+
+data Position = Position {_location :: Location
+                         , _angle :: Angle
+                         , _heading :: Heading
+                         , _posConfidence :: PosConf
+                         }
               deriving (Eq, Show)
                        
 $(makeLenses ''Position)
 
+type TrackSpec = Graph 
+
+data LinearPos = LinearPos {
