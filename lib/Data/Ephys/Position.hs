53c5ab8367d5d812f1d9e8493637109b75dabff9
Greg Hale <imalsogreg@gmail.com>
2013-11-02 08:47:20 -0400

    Settling on types for representing a track.  Convenience function to build a circular track.  Placeholder for building a track from any track spec (a graph of labeled points)

--- lib/Data/Ephys/Position.hs
+++ lib/Data/Ephys/Position.hs
@@ -6,7 +6,7 @@ import Data.Ephys.EphysDefs
 
 import Data.Graph
 import Control.Lens
-import Data.Trees.KdTree
+import qualified Data.Trees.KdTree as KD
 
 data Location = Location {_x :: Double, _y :: Double, _z :: Double}
               deriving (Eq, Show)
@@ -36,11 +36,27 @@ $(makeLenses ''Position)
 data TrackBin = TrackBin { _binNam :: String
                          , _binX   :: Double
                          , _binY   :: Double
+                         , _binDir :: Double -- radians
+                         , _binA   :: Double --
+                         , _binZ   :: Double
                          } deriving (Eq, Show)
 
-data TrackSpec = TrackSpek { _keyPoints :: Graph }  -- node :: (x,y), key :: String
+$(makeLenses ''TrackBin)
 
-data Track = Track { _trackBins :: KdTree TrackBin
+-- TODO - risky to depend on KdTree?  Not very active package
+instance KD.Point TrackBin where
+  dimension   = const 2
+  coord 0     = _binX
+  coord 1     = _binY
+  coord 2     = const 0
+  coord n     = error $
+                "Impossible coordinate from TrackBin: " ++ show n
+  dist2 b1 b2 = (b2 ^. binX - b1 ^. binX)^(2::Int) +
+                   (b2 ^. binY - b1 ^. binY)^(2::Int)
+
+data TrackSpec = TrackSpec { _keyPoints :: Graph }  -- node :: (x,y), key :: String
+
+data Track = Track { _trackBins :: KD.KdTree TrackBin
                    , _trackWid  :: Double
                    } deriving (Eq, Show)
 
@@ -50,3 +66,32 @@ data TrackDirection = Outbound | Inbound
 data TrackPos = TrackPos { _trackBin :: TrackBin
                          , _trackDir :: TrackDirection
                          } deriving (Eq, Show)
+
+$(makeLenses ''TrackSpec)
+$(makeLenses ''Track)
+$(makeLenses ''TrackPos)
+
+trackFromSpec :: TrackSpec 
+                 -> Double -- track width in metres
+                 -> Double -- bin length in meters
+                 -> Track
+trackFromSpec = undefined
+
+circularTrack :: (Double,Double) -- (x,y) in meters
+                 -> Double       -- radius in meters
+                 -> Double       -- track width in meters
+                 -> Double       -- bin length in meters
+                 -> Track
+circularTrack (cX,cY) r w tau =
+  let fI = fromIntegral
+      diam = 2*pi*r
+      nPts = floor (diam / tau) :: Int
+      tau' = diam / fI nPts
+      thetaCs = [0, tau' .. (diam - tau')]
+      aPoint :: Double -> TrackBin
+      aPoint theta = TrackBin ""
+                     (r * cos theta + cX) (r * sin theta + cY)
+                     (theta + pi/2)
+                     (-1 * tau / 2) (tau / 2) in
+  Track (KD.fromList [aPoint t | t <- thetaCs]) w
+                     
