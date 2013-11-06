4c03582d4d762f74443f0d62e494a241f73aab90
Greg Hale <imalsogreg@gmail.com>
2013-11-06 08:44:29 -0500

    functions to accumulate position for heading and speed, and to convert from mwl position to arte.  Not testet

--- lib/Data/Ephys/OldMWL/ParsePFile.hs
+++ lib/Data/Ephys/OldMWL/ParsePFile.hs
@@ -28,18 +28,55 @@ data MWLPos = MWLPos { _mwlPosTime  :: Double
 
 $(makeLenses ''MWLPos)
 
-produceMWLPos :: BSL.ByteString ->
+produceMWLPos :: Double -> Double -> Double -> BSL.ByteString ->
                  Producer MWLPos IO (Either (PBinary.DecodingError,
                                              Producer BS.ByteString IO ()) ())
-produceMWLPos f = 
+produceMWLPos pX0 pY0 pixPerMeter f = 
   PBinary.decodeGetMany parsePRecord (PBS.fromLazy . dropHeaderInFirstChunk $ f) >-> PP.map snd
 
-parsePRecord :: Binary.Get MWLPos
-parsePRecord = do
-  recTs    <- getWord32le
-  recXf    <- getWord16le
-  recYf    <- getWord16le
-  recXb    <- getWord16le
-  recYb    <- getWord16le
-  return $ MWLPos (decodeTime recTs)
-    (fromIntegral recXf) (fromIntegral recYf) (fromIntegral recXb) (fromIntegral recYb)
+parsePRecord :: Double -> Double -> Double -> Binary.Get MWLPos
+parsePRecord pX0 pY0 pixPerMeter =
+  let s = 1/pixPerMeter
+      pXToArte = (*s) . (subtract pX0)
+      pYToArte = (*s) . (subtract pY0)
+  in  do
+    recTs    <- getWord32le
+    recXf    <- getWord16le
+    recYf    <- getWord16le
+    recXb    <- getWord16le
+    recYb    <- getWord16le
+    return $ MWLPos (decodeTime recTs)
+      (fI recXf)
+      (fI recYf)
+      (fI recXb)
+      (fI recYb)
+
+mwlToArtePos :: (Double,Double)
+             -> Double
+             -> Double
+             -> MWLPos
+             -> Position
+             -> Position
+mwlToArtePos (pX0,pY0) pixelsPerMeter height m p =
+  let s = 1/pixelsPerMeter
+      pXToArte = (*s) . (subtract pX0)
+      pYToArte = (*s) . (subtract pY0)
+      fX = fI $ m^.mwlPxf :: Double
+      fY = fI $ m^.mwlPyf :: Double
+      bX = fI $ m^.mwlPxb :: Double
+      bY = fI $ m^.mwlPyb :: Double
+      x = pXToArte $ avg2 fX bX
+      y = pXToArte $ avg2 fY bY
+      loc = Location x y height
+      angle = Angle (atan2 (fY - bY) (fX - bX)) 0 0
+      conf = if all (> 0) [fX,bX,fY,bY] then ConfSure else ConfUnsure
+  in
+  stepPos p (m^.mwlPosTime) loc angle conf
+
+
+avg2 :: Double -> Double -> Double
+avg2 a b = (a+b)/2
+
+
+fI :: (Num a, Integral b) => b -> a
+fI = fromIntegral
--- lib/Data/Ephys/Position.hs
+++ lib/Data/Ephys/Position.hs
@@ -24,18 +24,43 @@ data PosConf = ConfNone | ConfUnsure | ConfSure
              deriving (Eq, Ord, Show)
 
 -- Full 3D position data.  For position relative to a linear track, see TrackPos
-data Position = Position { _posTime       :: ExperimentTime
-                         , _location     :: Location
-                         , _angle         :: Angle
-                         , _heading       :: Double
-                         , _speed         :: Double
-                         , _posConfidence :: PosConf
+data Position = Position { _posTime        :: ExperimentTime
+                         , _location       :: Location
+                         , _angle          :: Angle
+                         , _heading        :: Double
+                         , _speed          :: Double
+                         , _posConfidence  :: PosConf
+                         , _headingHistory :: [Double]
+                         , _speedHistory   :: [Double]
                          }
               deriving (Eq, Ord, Show)
 
 $(makeLenses ''Location)
 $(makeLenses ''Angle)
 $(makeLenses ''Position)
+
+stepPos :: Position
+        -> ExperimentTime
+        -> Location
+        -> Angle
+        -> PosConf
+        -> Position
+stepPos p t loc ang conf =
+  Position t loc ang heading' speed' conf hHist' sHist'
+  where dt = t - p^.posTime
+        (dx,dy,_) = locDiff (p^.location) loc
+        instantHeading = atan2 dy dx  --TODO: is this right?
+        hHist'         = instantHeading : init (p^.headingHistory)
+        heading'       = circMean hHist'
+        instantSpeed   = locDist (p^.location) loc / dt
+        sHist'         = instantSpeed : init (p^.speedHistory)
+        speed'         = mean sHist'
+
+locDiff :: Location -> Location -> (Double,Double,Double)
+locDiff a b = (dx, dy, dz)
+  where dx = b^.x - a^.x
+        dy = b^.y - a^.y
+        dz = b^.z - a^.z
 
 locSqDist :: Location -> Location -> Double
 locSqDist a b = (b^.z - a^.z)^(2::Int)
@@ -56,3 +81,12 @@ instance KD.Point Location where
   dist2 a1 a2 = (a2 ^. x - a1 ^. x)^(2::Int) +
                 (a2 ^. y - a1 ^. y)^(2::Int) +
                 (a2 ^. z - a1 ^. z)^(2::Int)
+
+mean :: [Double] -> Double
+mean xs = sum xs / (fromIntegral . length $ xs)
+
+circMean :: [Double] -> Double
+circMean angs =
+  let mrvX = mean . map cos $ angs
+      mrvY = mean . map sin $ angs
+  in atan2 mrvY mrvX  -- TODO - is this right?
