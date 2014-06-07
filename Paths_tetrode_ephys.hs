d23e54f46a1c8dcb10a2a8fd38b0a5170c1db24c
Greg Hale <imalsogreg@gmail.com>
2014-06-06 21:31:28 -0400

    sync before home

---
+++ Paths_tetrode_ephys.hs
@@ -0,0 +1,5 @@
+module Paths_tetrode_ephys  where
+import Data.Version
+
+version :: Version
+version = Version {versionBranch = [0,1,0], versionTags = []}
--- utils/drawKD.hs
+++ utils/drawKD.hs
@@ -1,27 +1,40 @@
 module Main where
 
 import Control.Applicative
+import Control.Concurrent.Async
+import Control.Concurrent.STM
 import Control.Monad
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
+import Data.List (foldl')
 import Data.Map.KDMap
 import Data.Monoid
+import Data.Vector ((!))
 import Debug.Trace
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
+import Pipes
+import qualified Pipes.Prelude as PP
+
+import Data.Ephys.Spike
+import Data.Ephys.OldMWL.Parse
 
 drawPoint :: Point2 -> Picture
 drawPoint (Point2 x y w) =
-  translate (realToFrac x) (realToFrac y) $ circle (realToFrac w)
+  translate (realToFrac x) (realToFrac y) $ circle (log (realToFrac w) / 10000000)
 
 data World = World { mainMap   :: KDMap Point2 Int
                    , selection :: Maybe (Point2, Int)
+                   , time      :: Double
+                   , spikeChan :: TChan TrodeSpikes
                    }
 
 instance Monoid Int where
   mempty = 0
   mappend = (+)
 
-world0 :: World
-world0 = World KDEmpty Nothing
+world0 :: Producer TrodeSpike IO () -> World
+world0 p = World KDEmpty Nothing 4492 p
 
 drawTree :: KDMap Point2 a -> Picture
 drawTree = Pictures . map (drawPoint) . keys
@@ -31,36 +44,38 @@ drawSelection Nothing = []
 drawSelection (Just ((Point2 x y w),i)) = [translate (realToFrac x) (realToFrac y) $ circleSolid (realToFrac w)]
 
 drawWorld :: World -> IO Picture
-drawWorld w = return $ Pictures (drawTree (mainMap w) :
-                                 drawSelection (selection w))
+drawWorld w = return $ scale 3000000 3000000 $
+              Pictures (drawTree (mainMap w) : drawSelection (selection w))
 
 fTime :: Float -> World -> IO World
-fTime _ w = return w
+fTime t w = do
+  let tNext = time w + realToFrac t
+  spikes <- while (\s -> spikeTime s < tNow) 
+  let spikePoints s = (spikeAmplitudes s ! 0, spikeAmplitudes s ! 1)
+      toPoint (x,y) = Point2 (realToFrac x) (realToFrac y) 1.0
+      points = map (toPoint . spikePoints) spikes
+      newMap = foldl' (\m p -> add m 0.00001 p 10) (mainMap w) points
+--  print $ points
+  return w { mainMap = newMap,
+             time = tNext
+           }
+
 
 ------------------------------------------------------------------------------
 fInputs :: Event -> World -> IO World
 fInputs (EventKey (MouseButton b) Up _ (x,y)) w
   | b == LeftButton =
-    let newMap = add (mainMap w) 100.0 (Point2 (realToFrac x) (realToFrac y) 3.0) 10
+    let newMap = add (mainMap w) 20.0 (Point2 (realToFrac x) (realToFrac y) 3.0) 10
     in  return w { mainMap = newMap }
   | otherwise = return w
 fInputs _ w = return w
-{-
-  = do
-  let newMap = insert 0 (Point2 (realToFrac x) (realToFrac y) 1.0) 10 (mainMap w)
-  unless (isValid newMap) (putStrLn $ "INVALID after insert: " ++ show newMap)
-  return $ w { mainMap = newMap }
-fInputs (EventKey (MouseButton RightButton) Up _ (x,y)) w =
-  return $ w { selection = closest (Point2 (realToFrac x) (realToFrac y) 5) (mainMap w)}
-fInputs (EventKey (MouseButton MiddleButton) Up _ (x,y)) w = do
-  let newMap = maybe (mainMap w) (\p -> delete (fst p) (mainMap w))
-               (closest (Point2 (realToFrac x) (realToFrac y) 1.0) (mainMap w))
-  unless (isValid newMap) (print $ "INVALID after delete: " ++ show newMap)
-  return $ w { selection = Nothing, mainMap = newMap }
--}
---fInputs _ w = return w
+
 
 ------------------------------------------------------------------------------
 main :: IO ()
-main = playIO (InWindow "KDMap" (500,500) (100,100))
-       white 30 world0 drawWorld fInputs fTime
+main = do
+  let fn = "/home/greghale/Data/caillou/112812clip2/1628/1628.tt"
+  let p = produceTrodeSpikesFromFile fn 16
+  _ <- async $ prod
+  playIO (InWindow "KDMap" (500,500) (100,100))
+    white 30 (world0 fi bytes) drawWorld fInputs fTime
