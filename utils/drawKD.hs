6d5410090992b2ef4ea0c76fe3eafd34fab686c1
Greg Hale <ImAlsoGreg@gmail.com>
2014-06-08 00:51:25 -0400

    nice colors

--- utils/drawKD.hs
+++ utils/drawKD.hs
@@ -6,12 +6,14 @@ import Control.Concurrent.STM
 import Control.Monad
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BSL
+import Data.Function (on)
 import Data.List (foldl')
 import Data.Map.KDMap
 import Data.Monoid
 import Data.Vector ((!))
 import Debug.Trace
 import Graphics.Gloss
+import qualified Graphics.Gloss.Data.Color as Color
 import Graphics.Gloss.Interface.IO.Game
 import Pipes
 import Pipes.RealTime
@@ -21,33 +23,50 @@ import System.Environment
 import Data.Ephys.Spike
 import Data.Ephys.OldMWL.Parse
 
-drawPoint :: Point2 -> Picture
-drawPoint (Point2 x y w) =
-  translate (realToFrac x) (realToFrac y) $ circle (log (realToFrac w) / 3000000)
+drawPoint :: Double -> (Point2, SpikeTime) -> Picture
+drawPoint tNow (Point2 x y w, tSpike) =
+  translate (realToFrac x) (realToFrac y) $
+  Pictures [ color (pointColor tNow tSpike) $ circleSolid (1e-6 + log (realToFrac w) / 3000000)
+           , circle (1e-6 + log (realToFrac w) / 3000000)
+           ]
 
-data World = World { mainMap   :: KDMap Point2 Int
-                   , selection :: Maybe (Point2, Int)
+pointColor :: Double -> SpikeTime -> Color.Color
+pointColor tNow (SpikeTime tSpike) = Color.makeColor r g b 1
+  where e tau = exp (-1 * (max dt 0) / tau)
+        dt = (realToFrac tNow) - (realToFrac tSpike)
+        r = e 1
+        g = e 0.5
+        b = e 20
+
+newtype SpikeTime = SpikeTime { unSpikeTime :: Double }
+                  deriving (Eq)
+
+instance Monoid SpikeTime where
+  mempty = SpikeTime (-1e6)
+  mappend a b = SpikeTime $ (max `on` unSpikeTime) a b
+
+
+
+data World = World { mainMap   :: KDMap Point2 SpikeTime
+                   , selection :: Maybe (Point2, SpikeTime)
                    , time      :: Double
                    , spikeChan :: TChan TrodeSpike
                    }
 
-instance Monoid Int where
-  mempty = 0
-  mappend = (+)
-
 world0 :: TChan TrodeSpike -> World
 world0 c = World KDEmpty Nothing 4492 c
 
-drawTree :: KDMap Point2 a -> Picture
-drawTree = Pictures . map (drawPoint) . keys
+drawTree :: Double -> KDMap Point2 SpikeTime -> Picture
+drawTree tNow = Pictures . map (drawPoint tNow) . toList
 
-drawSelection :: Maybe (Point2,Int) -> [Picture]
+drawSelection :: Maybe (Point2,SpikeTime) -> [Picture]
 drawSelection Nothing = []
-drawSelection (Just ((Point2 x y w),i)) = [translate (realToFrac x) (realToFrac y) $ circleSolid (realToFrac w)]
+drawSelection (Just ((Point2 x y w),i)) = [translate (realToFrac x) (realToFrac y) $
+                                           circleSolid (realToFrac w)]
 
 drawWorld :: World -> IO Picture
 drawWorld w = return $ translate (-200) (-200) $ scale 2000000 2000000 $
-              Pictures (drawTree (mainMap w) : drawSelection (selection w))
+              Pictures (drawTree (time w) (mainMap w) : drawSelection (selection w))
 
 flushChan :: TChan a -> STM [a]
 flushChan c = go []
@@ -67,7 +86,8 @@ fTime t w = do
   let spikePoints s = (spikeAmplitudes s ! 0, spikeAmplitudes s ! 1)
       toPoint (x,y) = Point2 (realToFrac x) (realToFrac y) 1.0
       points = map (toPoint . spikePoints) spikes
-      newMap = foldl' (\m p -> add m 0.000007 p 10) (mainMap w) points
+      times  = map (SpikeTime . spikeTime) spikes
+      newMap = foldl' (\m (p,t) -> add m 0.000007 p t) (mainMap w) (zip points times)
   return w { mainMap = newMap,
              time = tNext
            }
@@ -77,7 +97,7 @@ fTime t w = do
 fInputs :: Event -> World -> IO World
 fInputs (EventKey (MouseButton b) Up _ (x,y)) w
   | b == LeftButton =
-    let newMap = add (mainMap w) 20.0 (Point2 (realToFrac x) (realToFrac y) 3.0) 10
+    let newMap = add (mainMap w) 20.0 (Point2 (realToFrac x) (realToFrac y) 3.0) (SpikeTime 0)
     in  return w { mainMap = newMap }
   | otherwise = return w
 fInputs _ w = return w
